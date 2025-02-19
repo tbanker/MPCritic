63fe7a2c945a39a5e3c7b59bb959e62c9a3ff896
tbanker <thomas_banker@berkeley.edu>
2025-02-18 16:16:50 -0800

    Weak Q-learning sketch

--- main_neuromancer.py
+++ main_neuromancer.py
@@ -83,25 +83,23 @@ class InputConcat(torch.nn.Module):
         self.module = module
 
     def forward(self, x, u):
-        """ f(z) <- f(x,u) """
+        """ f(x,u) -> f(z) """
         # L4CasADi models must have 1 input (z), but Neuromancer needs 2 (x,u)
         z = torch.concat((x,u), dim=-1)
         return self.module(z)
 
-# closed-loop system
-concat_dynamics = InputConcat(critic.mpc_model)
+""" Closed-loop 'system' """
 concat_cost = InputConcat(critic.mpc_lterm)
 stage_cost = Node(concat_cost, ['X', 'U'], ['l'], name='stage_cost')
 terminal_cost = Node(critic.mpc_mterm, ['X'], ['V'], name='terminal_cost')
 
+concat_dynamics = InputConcat(critic.mpc_model)
 linear_dynamics = Node(concat_dynamics, ['X', 'U'], ['X'], name='linear_dynamics')
 policy = Node(critic.dpc, ['X'], ['U'], name='policy')
 
-'''
-mpc_settings['n_horizon']+1 redundant terminal cost evaluations
-1 redundant stage cost evaluations
-but objective matches that of do-mpc, horizon considered :)
-'''
+# mpc_settings['n_horizon']+1 redundant terminal cost evaluations
+# 1 redundant stage cost evaluations
+# but objective matches that of do-mpc, horizon considered :)
 cl_system = System([policy, stage_cost, linear_dynamics, terminal_cost])
 cl_system.nsteps = critic.mpc_settings['n_horizon'] + 2
 
@@ -113,75 +111,81 @@ train_loader = torch.utils.data.DataLoader(train_data, batch_size=3333,
 dev_loader = torch.utils.data.DataLoader(dev_data, batch_size=3333,
                                          collate_fn=dev_data.collate_fn, shuffle=False)
 
-# objective and optimizer
+""" Objective and optimizer """
 u = variable('U')
 x = variable('X')
 l = variable('l')
 V = variable('V')
 
-'''
-Ignore last stage cost (k=mpc_settings['n_horizon']+1), only consider last temrinal cost (k=mpc_settings['n_horizon']+2).
-Stage cost should be weighted (mpc_settings['n_horizon']+1) times more than terminal cost.
-This is because Neuromancer computes mean(stage costs) rather than mean(sum of stage_costs over horizon) for this formulation.
-'''
-stage_loss = (critic.mpc_settings['n_horizon']+1.) * (l[:, :-1, :] == 0.)
-terminal_loss = 1. * (V[:, [-1], :] == 0.)
-stage_loss.name = "stage_loss"
-terminal_loss.name = "terminal_loss"
-
+# Ignore last stage cost (k=mpc_settings['n_horizon']+1), only consider last temrinal cost (k=mpc_settings['n_horizon']+2).
+# Stage cost should be weighted (mpc_settings['n_horizon']+1) times more than terminal cost.
+# This is because Neuromancer computes mean(stage costs) rather than mean(sum of stage_costs over horizon) for this formulation.
+stage_loss = Objective(var=(critic.mpc_settings['n_horizon']+1.)*l[:, :-1, :], name="stage_loss")
+terminal_loss = Objective(var=(V[:, [-1], :]), name="terminal_loss")
 loss = PenaltyLoss([stage_loss, terminal_loss], [])
+
 problem = Problem([cl_system], loss)
 optimizer = torch.optim.AdamW(policy.parameters(), lr=0.01)
 
-# learning
-trainer = Trainer(
-    problem,
-    train_loader,
-    dev_loader,
-    dev_loader,
-    optimizer=optimizer,
-    epochs=10,
-    train_metric="train_loss",
-    dev_metric="dev_loss",
-    eval_metric='dev_loss',
-    warmup=4,
-)
+if learn:
+    """ Learning ficticious controller with prediction horizon of 10 """
+    trainer = Trainer(
+        problem,
+        train_loader,
+        dev_loader,
+        dev_loader,
+        optimizer=optimizer,
+        epochs=1000,
+        train_metric="train_loss",
+        dev_metric="dev_loss",
+        eval_metric='dev_loss',
+        warmup=200,
+    )
 
-# Train model with prediction horizon of 10
-best_model = trainer.train()
-K_opt = calc_K(A_mpc, B_mpc, Q, R).astype(np_kwargs['dtype'])
-print(f"Distance from optimal gain: {np.linalg.norm(K_opt - K, 'fro')}")
+    best_model = trainer.train()
+    K_opt = calc_K(A_mpc, B_mpc, Q, R).astype(np_kwargs['dtype'])
+    print(f"'Distance' from optimal gain: {np.linalg.norm(K_opt - K, 'fro')}")
 
-# Evaluating loss function for random inputs
-loss_dict = {k: torch.randn(20, cl_system.nsteps, 1) for k in loss.input_keys}
-out_dict = loss(loss_dict)
+else:
+    """ Evaluating loss function for random inputs """
+    loss_dict = {k: torch.randn(20, cl_system.nsteps, 1) for k in loss.input_keys}
+    out_dict = loss(loss_dict)
 
-# Evaluating loss function for random initial states
-eval_data = DictDataset({"X" : 3.*torch.randn(3333, 1, n),
-                         "U"  : 3.*torch.randn(3333, 1, m),
-                         "R"  : 3.*torch.randn(3333, 1, 1),
-                         "X'" : 3.*torch.randn(3333, 1, n),}, name='train')
-eval_buffer = torch.utils.data.DataLoader(eval_data, batch_size=64,
-                                          collate_fn=train_data.collate_fn, shuffle=False)
+    """ Evaluating Q-function for random tuples, presumably coming from replay buffer """
+    eval_data = DictDataset({"X" : 3.*torch.randn(1024, 1, n),
+                             "U"  : 3.*torch.randn(1024, 1, m),
+                             "R"  : 3.*torch.randn(1024, 1, 1),
+                             "X'" : 3.*torch.randn(1024, 1, n),}, name='train')
+    eval_buffer = torch.utils.data.DataLoader(eval_data, batch_size=64,
+                                            collate_fn=train_data.collate_fn, shuffle=False)
 
-def get_subdict(dict, keys, rekey=False, val=None):
-    subdict = {key : dict[key] for key in keys}
-    if rekey and (val != None):
-        subdict["X"] = val
-    elif rekey:
-        subdict["X"] = subdict.pop("X'")
-    return subdict
+    def get_subdict(dict, keys, rekey=False, val=None):
+        """ Extract sub-dictionary of dict & rekey either ["X'"] or val to ["X"] """
+        subdict = {key : dict[key] for key in keys}
+        if rekey and (val != None):
+            subdict["X"] = val
+        elif rekey:
+            subdict["X"] = subdict.pop("X'")
+        return subdict
 
-for batch in eval_buffer:
-    # go through and validate gradient tracking
-    q_x_next = problem(get_subdict(batch, ["X'", "name"], rekey=True))['train_loss']
-    q_pred = torch.mean(batch['R']) + q_x_next
+    """ Bad steps for Q-learning! """
+    critic.requires_grad_(True)
+    for batch in eval_buffer:
+        with torch.no_grad():
+            # cost of ficticious controller rollout for cl_system.nsteps from s'
+            q_x_next = problem(get_subdict(batch, ["X'", "name"], rekey=True))['train_loss']
+            # bad target Q-value for (r, s')
+            q_targ = torch.mean(batch['R']) + q_x_next # ||x+y|| <= ||x|| + ||y|| :(
 
-    cl_system.nsteps -= 1
-    z = torch.concat((batch['X'], batch['U']), dim=-1).squeeze(1)
-    q_l = torch.mean(critic.mpc_lterm(z))
-    x_pred = critic.mpc_model(z).unsqueeze(1)
-    q_x = problem(get_subdict(batch, ["X", "name"], rekey=True, val=x_pred))['train_loss']
-    cl_system.nsteps += 1
+        # stage cost of (s,a)
+        z = torch.concat((batch['X'], batch['U']), dim=-1).squeeze(1)
+        q_l = torch.mean(critic.mpc_lterm(z))
 
-    q_targ = q_l + q_x
+        # cost of ficticious controller rollout for (cl_system.nsteps-1) from predicted s'
+        cl_system.nsteps -= 1
+        x_pred = critic.mpc_model(z).unsqueeze(1)
+        q_x = problem(get_subdict(batch, ["X", "name"], rekey=True, val=x_pred))['train_loss']
+        # bad predicted Q-value for (s, a)
+        q_pred = q_l + q_x # ||x+y|| <= ||x|| + ||y|| :(
+
+        cl_system.nsteps += 1
