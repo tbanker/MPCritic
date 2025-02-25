c7761d165db8e558d8d689e3d48d0cb0d8d4b12a
tbanker <thomas_banker@berkeley.edu>
2025-02-24 17:32:56 -0800

    Dynamics example with dx as input

--- modules/dynamics.py
+++ modules/dynamics.py
@@ -18,8 +18,12 @@ from neuromancer.psl import signals
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
+np_kwargs = {'dtype' : np.float32}
+kwargs = {'dtype' : torch.float32,
+          'device' : 'cpu'}
+
 class Dynamics(nn.Module):
-    def __init__(self, env, rb):
+    def __init__(self, env, rb, dx=None):
         super().__init__()
 
         self.env = env
@@ -28,12 +32,15 @@ class Dynamics(nn.Module):
         # Configure network
         self.nx = np.array(env.single_observation_space.shape).prod()
         self.nu = np.array(env.single_action_space.shape).prod()
-        self.dx = blocks.ResMLP(self.nx + self.nu, self.nx, bias=True, linear_map=torch.nn.Linear, nonlin=torch.nn.SiLU,
-                    hsizes=[64 for h in range(2)])
+        self.dx = dx if dx != None else blocks.ResMLP(self.nx + self.nu, self.nx, bias=True,
+                                                      linear_map=torch.nn.Linear, nonlin=torch.nn.SiLU,
+                                                      hsizes=[64 for h in range(2)])
         self.system_node = Node(self.dx, ['x','u'],['xnext'])
         self.x_shift = Node(lambda x: x, ['xnext'], ['x'])
         self.model = System([self.system_node], nstep_key='u') # or nsteps=1
         self.model_eval = System([self.system_node, self.x_shift], nstep_key='u')
+        self.model_kwargs = {'dtype' : list(self.model.parameters())[0].dtype,
+                             'device' : list(self.model.parameters())[0].device,}
 
         # Formulate problem
         self.xpred = variable('xnext')
@@ -63,9 +70,9 @@ class Dynamics(nn.Module):
         # need to coordinate the number of epoch and batch size
         batch = self.rb.sample(1000)
         data = {}
-        data['x'] = batch.observations.unsqueeze(1)
-        data['u'] = batch.actions.unsqueeze(1)
-        data['xtrue'] = batch.next_observations.unsqueeze(1)
+        data['x'] = batch.observations.unsqueeze(1).to(**self.model_kwargs)
+        data['u'] = batch.actions.unsqueeze(1).to(**self.model_kwargs)
+        data['xtrue'] = batch.next_observations.unsqueeze(1).to(**self.model_kwargs)
         datadict = DictDataset(data)
 
         train_loader = DataLoader(datadict, batch_size=64, shuffle=True, collate_fn=datadict.collate_fn)
@@ -77,7 +84,8 @@ class Dynamics(nn.Module):
 
         obs, info = self.env.reset()
         signal = signals.prbs(10, self.nu, min=self.env.action_space.low, max=self.env.action_space.high, p=.9, rng=np.random.default_rng())/2
-        trajectory = self.model_eval({'x': torch.Tensor(obs).unsqueeze(1), 'u':torch.Tensor(signal[None,:])})   
+        trajectory = self.model_eval({'x': torch.Tensor(obs).unsqueeze(1).to(**self.model_kwargs),
+                                      'u':torch.Tensor(signal[None,:]).to(**self.model_kwargs)})   
         
         mae = 0.0
         for u,x in zip(signal,trajectory['x'].squeeze().detach().numpy()):
@@ -88,4 +96,142 @@ class Dynamics(nn.Module):
 
         print(mae/len(signal))
         return
-        
+
+if __name__ == "__main__":
+    import os
+    import random
+    import time
+    from dataclasses import dataclass
+    import tyro
+
+    from stable_baselines3.common.buffers import ReplayBuffer
+
+    from mpcritic import InputConcat
+    from mpcomponents import LinearDynamics
+
+    device = kwargs['device']
+    
+    """ CleanRL setup """
+    @dataclass
+    class Args:
+        exp_name: str = os.path.basename(__file__)[: -len(".py")]
+        """the name of this experiment"""
+        seed: int = 1
+        """seed of the experiment"""
+        torch_deterministic: bool = True
+        """if toggled, `torch.backends.cudnn.deterministic=False`"""
+        cuda: bool = True
+        """if toggled, cuda will be enabled by default"""
+        track: bool = False
+        """if toggled, this experiment will be tracked with Weights and Biases"""
+        wandb_project_name: str = "cleanRL"
+        """the wandb's project name"""
+        wandb_entity: str = None
+        """the entity (team) of wandb's project"""
+        capture_video: bool = False
+        """whether to capture videos of the agent performances (check out `videos` folder)"""
+        save_model: bool = False
+        """whether to save model into the `runs/{run_name}` folder"""
+        upload_model: bool = False
+        """whether to upload the saved model to huggingface"""
+        hf_entity: str = ""
+        """the user or org name of the model repository from the Hugging Face Hub"""
+
+        # Algorithm specific arguments
+        env_id: str = "Hopper-v4"
+        """the environment id of the Atari game"""
+        total_timesteps: int = 10000
+        """total timesteps of the experiments"""
+        learning_rate: float = 3e-4
+        """the learning rate of the optimizer"""
+        buffer_size: int = int(1e6)
+        """the replay memory buffer size"""
+        gamma: float = 0.99
+        """the discount factor gamma"""
+        tau: float = 0.005
+        """target smoothing coefficient (default: 0.005)"""
+        batch_size: int = 256
+        """the batch size of sample from the reply memory"""
+        exploration_noise: float = 0.1
+        """the scale of exploration noise"""
+        learning_starts: int = 25e3
+        """timestep to start learning"""
+        policy_frequency: int = 2
+        """the frequency of training policy (delayed)"""
+        noise_clip: float = 0.5
+        """noise clip parameter of the Target Policy Smoothing Regularization"""
+
+    def make_env(env_id, seed, idx, capture_video, run_name):
+        def thunk():
+            if capture_video and idx == 0:
+                env = gym.make(env_id, render_mode="rgb_array")
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+            else:
+                env = gym.make(env_id)
+            env = gym.wrappers.RecordEpisodeStatistics(env)
+            env.action_space.seed(seed)
+            return env
+
+        return thunk
+
+    args = tyro.cli(Args)
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    envs = gym.vector.SyncVectorEnv([make_env(args.env_id, args.seed, 0, args.capture_video, run_name)])
+
+    rb = ReplayBuffer(
+        args.buffer_size,
+        envs.single_observation_space,
+        envs.single_action_space,
+        device,
+        handle_timeout_termination=False,
+    )
+
+    """ Fill replay buffer """
+    obs, _ = envs.reset(seed=args.seed)
+    for global_step in range(args.total_timesteps):
+        # ALGO LOGIC: put action logic here
+        actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, rewards, terminations, truncations, infos = envs.step(actions)
+
+        # TRY NOT TO MODIFY: save data to reply buffer; handle `final_observation`
+        real_next_obs = next_obs.copy()
+        for idx, trunc in enumerate(truncations):
+            if trunc:
+                real_next_obs[idx] = infos["final_observation"][idx]
+        rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
+
+        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+        obs = next_obs
+
+    """ Dynamics setup """
+    b, n, m = envs.num_envs, envs.single_observation_space.shape[0], envs.single_action_space.shape[0]
+
+    A = np.random.uniform(-1., 1., (n,n)).astype(np_kwargs['dtype'])
+    B = np.random.uniform(-1., 1., (n,m)).astype(np_kwargs['dtype'])
+    
+    l4c_model = LinearDynamics(n, m, A, B)
+    concat_dynamics = InputConcat(l4c_model)
+    dynamics = Dynamics(envs, rb, dx=concat_dynamics)
+
+    """ Model predictions """
+    action = envs.action_space.sample()
+    pred_dx = dynamics.dx(torch.from_numpy(obs).to(**kwargs), torch.from_numpy(action).to(**kwargs))
+    pred_dyn = dynamics(torch.from_numpy(obs).to(**kwargs), torch.from_numpy(action).to(**kwargs))
+    print(f"s'_dx == s'_dyn: {torch.allclose(pred_dx, pred_dyn)}")
+
+    """ Rollout eval """
+    dynamics.rollout_eval()
+
+    """ Training """
+    A_init = A.copy()
+    dynamics.train()
+    A_train = dynamics.dx.module.A.detach().numpy()
+    print(f"A' != A: {not np.allclose(A_init, A_train)}")
